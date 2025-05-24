3cb904746197f3c42b157127dd5c03d384db4ad1
Nofa Nisrina <nofanisrina@gmail.com>
2025-05-25 00:33:03 +0700

    feat: add cloudinary config

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dsi0bqoc8',
+    api_key: '237853841278558',
+    api_secret: '24MeyxnqYVru-GpnIMVutTJWO8U',
+});
+
+module.exports = cloudinary;
