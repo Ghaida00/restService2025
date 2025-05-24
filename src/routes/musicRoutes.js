bc3e4533edee0a2f033451bbaad50739c2b9a34a
Nofa Nisrina <nofanisrina@gmail.com>
2025-05-25 03:16:46 +0700

    feat: add routes get music by id

--- src/routes/musicRoutes.js
+++ src/routes/musicRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/multer');
-const { addMusic } = require('../controllers/musicController');
 
+const { 
+    addMusic,
+    getMusicById,
+    //janlup add nama method klen disini ges
+
+} = require('../controllers/musicController');
+
+// ===== BIKIN METHOD MULAI DI BAWAH INI YEAHHH ====
+
+// POST Music (Nofa)
 router.post('/', upload.fields([
     { name: 'music', maxCount: 1 },
     { name: 'cover', maxCount: 1 }
 ]), addMusic);
 
+// GET Music by ID (Nofa)
+router.get('/:id', getMusicById);
+
 module.exports = router;
+    
