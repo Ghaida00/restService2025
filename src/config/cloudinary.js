2f6d4619a51c309959729c20e8efcea6de8ca9aa
Nofa Nisrina <nofanisrina@gmail.com>
2025-05-25 02:11:15 +0700

    feat: add timeout 60secs for upload to cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,6 +4,7 @@ cloudinary.config({
     cloud_name: 'dsi0bqoc8',
     api_key: '237853841278558',
     api_secret: '24MeyxnqYVru-GpnIMVutTJWO8U',
+    timeout: 60000, // Set timeout to 60 seconds
 });
 
 module.exports = cloudinary;
